83d56bdbc4f65d09292449352867e10af2bcf3cf
keigo194547 <b1951154@planet.kanazawa-it.ac.jp>
2022-01-19 00:18:01 +0900

    ListView2項目表示

--- app/src/main/java/com/example/fitnessapp/BMIListInformation.java
+++ app/src/main/java/com/example/fitnessapp/BMIListInformation.java
@@ -3,6 +3,7 @@ package com.example.fitnessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -16,35 +17,42 @@ public class BMIListInformation extends AppCompatActivity {
 
     private static final String[] ITEMS = {"重度のやせ","中度のやせ","軽度のやせ",
             "普通","前肥満","肥満(1度)","肥満(2度)","肥満(3度)" };
+    static final String[] comments = {"重度のやせ","中度のやせ","軽度のやせ",
+            "普通","前肥満","肥満(1度)","肥満(2度)","肥満度(3度)"};
+    static final String[] valueBMI = {"16.0未満","16.0~17.0","17.0~18.5","18.5~25.0",
+            "25.0~30.0","30.0~35.0","35.0~40.0", "40以上"};
 
     ListView setList;
+
+    ArrayList<Map<String,String>> list = new ArrayList<Map<String,String>>();
+    SimpleAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmilist_information);
 
-        setContentView(R.layout.activity_bmilist_information);
+        setList = (ListView) findViewById(R.id.BMIList);
 
-        ArrayAdapter<String > adapter = new ArrayAdapter<String>(
-                this,R.layout.list_layout,ITEMS);
+        for(int i = 0; i < comments.length; i++){
+            Map<String, String> data = new HashMap<String,String>();
+            data.put("title", comments[i]);
+            data.put("explain", valueBMI[i]);
+            list.add(data);
+        }
 
-        setList = (ListView) findViewById(R.id.list);
+        adapter = new SimpleAdapter(
+                this,
+                list,
+                R.layout.list_layout,
+                new String[] {"title","explain"},
+                new int[] {R.id.title,R.id.explain}
+        );
 
         setList.setAdapter(adapter);
 
-
-
-
-
-
-
-
         /*
-        String[] comments = {"重度のやせ","中度のやせ","軽度のやせ",
-                "普通","前肥満","肥満(1度)","肥満(2度)","肥満(3度)" };
-        String[] valueBMI = {"16.0未満","16.0~17.0","17.0~18.5","18.5~25.0",
-                "25.0~30.0","30.0~35.0","35以上"};
+
 
         List<Map<String,String >> information = new ArrayList<Map<String ,String >>();
 
