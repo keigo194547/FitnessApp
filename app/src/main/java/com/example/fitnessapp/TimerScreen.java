6204ada0004b7d9f4474245dfc58975bacfbb2b8
keigo194547 <b1951154@planet.kanazawa-it.ac.jp>
2022-01-19 12:22:46 +0900

    画像切り替えTimer実装

--- app/src/main/java/com/example/fitnessapp/TimerScreen.java
+++ app/src/main/java/com/example/fitnessapp/TimerScreen.java
@@ -6,8 +6,12 @@ import android.os.Bundle;
 import android.widget.Chronometer;
 import android.os.SystemClock;
 import android.view.View;
+import android.widget.ImageView;
 
-public class TimerScreen extends AppCompatActivity {
+// 定期処理するためのメソッド仕様宣言
+public class TimerScreen extends AppCompatActivity implements Chronometer.OnChronometerTickListener {
+
+    int x = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +25,28 @@ public class TimerScreen extends AppCompatActivity {
     public void onStart( View v ){
         ((Chronometer)findViewById(R.id.chronometer)).setBase(SystemClock.elapsedRealtime());
         ((Chronometer)findViewById(R.id.chronometer)).start();
+
+        ((Chronometer)findViewById(R.id.chronometer)).setOnChronometerTickListener(this);
     }
 
     // ストップ
     public void onStop( View v ){
         ((Chronometer)findViewById(R.id.chronometer)).stop();
+        ((ImageView)findViewById(R.id.iv)).setImageResource(R.drawable.musle);
+    }
+
+    // 定期的処理② 約１秒毎に実行する具体的処理
+    @Override
+    public void onChronometerTick(Chronometer chronometer) {
+        // 条件分析
+        x++;    // xを+1させる
+        if( x % 2 ==0 ) {   // 偶数ですか？
+            // YESの場合
+            ((ImageView) findViewById(R.id.iv)).setImageResource(R.drawable.cute1);
+        }else {
+            // NOの場合
+            ((ImageView) findViewById(R.id.iv)).setImageResource(R.drawable.cute2);
+        }
     }
 
 }
